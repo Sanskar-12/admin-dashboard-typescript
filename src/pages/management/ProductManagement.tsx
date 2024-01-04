b16731605f6d411bbbe88ff54d0d410c00a7d78d
Sanskar-12 <sanskarv2004@gmail.com>
2024-01-04 22:40:09 +0530

    working

--- src/pages/management/ProductManagement.tsx
+++ src/pages/management/ProductManagement.tsx
@@ -1,12 +1,95 @@
-import AdminSidebar from "../../components/AdminSidebar"
+import { ChangeEvent, FormEvent, useState } from "react";
+import AdminSidebar from "../../components/AdminSidebar";
+
+const img =
+  "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=804";
+
 
 const ProductManagement = () => {
+
+  const [name,setName]=useState<string>("Puma Shoes")
+  const [price,setPrice]=useState<number>(1000)
+  const [stock,setStock]=useState<number>(20)
+  const [photo,setPhoto]=useState<string>(img)
+
+  const [nameUpdated,setNameUpdated]=useState<string>(name)
+  const [priceUpdated,setPriceUpdated]=useState<number>(price)
+  const [stockUpdated,setStockUpdated]=useState<number>(stock)
+  const [photoUpdated,setPhotoUpdated]=useState<string>(photo)
+
+  const changeImageHandler=(e:ChangeEvent<HTMLInputElement>)=>{
+    const file:File | undefined=e.target.files?.[0]
+
+    const reader:FileReader=new FileReader()
+
+    if(file)
+    {
+      reader.readAsDataURL(file)
+      reader.onloadend=()=>{
+        if(typeof reader.result==="string") setPhotoUpdated(reader.result)
+      }
+    }
+  }
+
+  const submitHandler=(e:FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+    setName(nameUpdated)
+    setPrice(priceUpdated)
+    setStock(stockUpdated)
+    setPhoto(photoUpdated)
+  }
+
   return (
     <div className="adminContainer">
       <AdminSidebar />
-      <main className="productManagement">sdf</main>
+      <main className="productManagement">
+
+        <section>
+          <strong>ID - sdfsdfksmdnfknsdkfns</strong>
+          <img src={photo} alt="Product" />
+          <p>{name}</p>
+          {
+            stock>0 ? (
+              <span className="green">{stock} Available</span>
+            ):(
+              <span className="red">Not Available</span>
+            )
+          }
+          <h3>${price}</h3>
+        </section>
+
+    <article>
+      <form onSubmit={(e)=>submitHandler(e)}>
+        <h2>Manage</h2>
+        <div>
+          <label>Name</label>
+          <input type="text" placeholder="Name" value={nameUpdated} onChange={(e)=>setNameUpdated(e.target.value)} required/>
+        </div>
+        <div>
+          <label>Price</label>
+          <input type="number" placeholder="Price" value={priceUpdated} onChange={(e)=>setPriceUpdated(Number(e.target.value))} required/>
+        </div>
+        <div>
+          <label>Stock</label>
+          <input type="number" placeholder="Stock" value={stockUpdated} onChange={(e)=>setStockUpdated(Number(e.target.value))} required/>
+        </div>
+        <div>
+          <label>Photo</label>
+          <input type="file" onChange={(e)=>changeImageHandler(e)} />
+        </div>
+
+        {
+          photoUpdated && <img src={photoUpdated} alt="New Image" />
+        }
+
+        <button type="submit">Update</button>
+      </form>
+    </article>
+
+      </main>
     </div>
-  )
-}
+  );
+};
+
 
 export default ProductManagement
--- src/pages/management/TransactionManagement.tsx
+++ src/pages/management/TransactionManagement.tsx
@@ -1,12 +1,127 @@
-import AdminSidebar from "../../components/AdminSidebar"
+import { useState } from "react";
+import AdminSidebar from "../../components/AdminSidebar";
+import { OrderItemType, OrderType } from "../../types";
+import { Link } from "react-router-dom";
+
+const img =
+  "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=804";
+
+const orderItems: OrderItemType[] = [
+  {
+    name: "Puma Shoes",
+    photo: img,
+    _id: "sdfsdf",
+    quantity: 4,
+    price: 2000,
+  },
+];
 
 const TransactionManagement = () => {
+  const [order, setOrder] = useState<OrderType>({
+    name: "Salman Khan",
+    address: "77 Black Street",
+    city: "Neyword",
+    state: "Nevada",
+    country: "India",
+    pinCode: 2434341,
+    status: "Processing",
+    subtotal: 4000,
+    discount: 1200,
+    shippingCharges: 0,
+    tax: 200,
+    total: 4000 + 200 + 0 - 1200,
+    orderItems,
+    _id: "asdnasjdhbn",
+  });
+
+  const {
+    name,
+    address,
+    city,
+    state,
+    country,
+    pinCode,
+    status,
+    subtotal,
+    discount,
+    shippingCharges,
+    tax,
+    total,
+  } = order;
+
+  const updateHandler = () => {
+    setOrder((prev) => ({
+      ...prev,
+      status: prev.status === "Processing" ? "Delivered" : "Shipped",
+    }));
+  };
+
   return (
     <div className="adminContainer">
-    <AdminSidebar />
-    <main className="productManagement">sdf</main>
-  </div>
-  )
-}
+      <AdminSidebar />
+      <main className="productManagement">
+        <section>
+          <h2>Order Items</h2>
 
-export default TransactionManagement
+          {order?.orderItems?.map((i) => (
+            <ProductCard
+              name={i?.name}
+              photo={i?.photo}
+              price={i?.price}
+              quantity={i?.quantity}
+              _id={i?._id}
+            />
+          ))}
+        </section>
+
+        <article className="shippingInfo">
+          <h1>Order Info</h1>
+          <h5>User Info</h5>
+          <p>Name: {name}</p>
+          <p>
+            Address: {`${address}, ${city}, ${state}, ${country} ${pinCode}`}
+          </p>
+
+          <h5>Amount Info</h5>
+          <p>Subtotal: {subtotal}</p>
+          <p>Shipping Charges: {shippingCharges}</p>
+          <p>Tax: {tax}</p>
+          <p>Discount: {discount}</p>
+          <p>Total: {total}</p>
+
+          <h5>Status Info</h5>
+          <p>
+            Status:{" "}
+            <span
+              className={
+                status === "Delivered"
+                  ? "purple"
+                  : status === "Shipped"
+                  ? "green"
+                  : "red"
+              }
+            >
+              {status}
+            </span>
+          </p>
+
+          <button onClick={updateHandler}>Process Status</button>
+        </article>
+      </main>
+    </div>
+  );
+};
+
+const ProductCard = ({ name, photo, price, quantity, _id }: OrderItemType) => {
+  return (
+    <div className="transactionProductCard">
+      <img src={photo} alt={name} />
+      <Link to={`/product/${_id}`}>{name}</Link>
+      <span>
+        ${price} X {quantity} = ${price * quantity}
+      </span>
+    </div>
+  );
+};
+
+export default TransactionManagement;
